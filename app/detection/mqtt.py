4dbbbd840f63137307d79aad532f04dc2e6df750
Sebastian Perdomo <sebastianperdomocardozo@hotmail.com>
2025-05-28 22:32:21 -0500

    Change: Se actualiza endpoint MQTT

--- app/detection/mqtt.py
+++ app/detection/mqtt.py
@@ -1,16 +1,32 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import JSONResponse
+from app.detection.services import detect_persons_haar_from_base64  # o mobilenet si prefieres
 import logging
 
 router = APIRouter()
 
 @router.post("/mqtt/message")
 async def receive_mqtt_message(request: Request):
-    body = await request.json()
-    logging.info(f"[MQTT] Mensaje recibido desde EMQX: {body}")
+    data = await request.json()
+    logging.info(f"[MQTT] Mensaje recibido desde EMQX: {data}")
 
-    # Aquí puedes hacer algo con el mensaje, por ejemplo:
-    topic = body.get("topic")
-    payload = body.get("payload")
+    payload = data.get("payload")
 
-    return JSONResponse(status_code=200, content={"status": "ok", "topic": topic, "payload": payload})
+    if not payload or "image_base64" not in payload:
+        return JSONResponse(status_code=400, content={"error": "Falta 'image_base64'"})
+
+    try:
+        result = detect_persons_haar_from_base64(payload["image_base64"])
+        return JSONResponse(
+            status_code=200,
+            content={
+                "message": "Detección ejecutada automáticamente",
+                "result": result
+            }
+        )
+    except Exception as e:
+        logging.error(f"[MQTT] Error al procesar la imagen: {e}")
+        return JSONResponse(
+            status_code=500,
+            content={"error": f"Error al ejecutar detección: {str(e)}"}
+        )
